801fa06b99c6628e3c41f1122324185ed801a386
alishah18105 <alishah18105@gmail.com>
2025-09-17 16:43:36 +0500

    Getting Forms Working

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -15,17 +15,28 @@ class Todo(db.Model):
     def __repr__(self):
         return f"{self.sno} {self.title}"
 
-@app.route("/")
+@app.route('/', methods=['GET', 'POST'] )
 def hello_world():
-    # todo = Todo(title = "First Task", desc = "Learn Flask")
-    # db.session.add(todo)
-    # db.session.commit()
+    if request.method == "POST":
+
+        title = request.form["title"]
+        desc = request.form["desc"]
+        todo = Todo(title = title, desc = desc)
+
+        db.session.add(todo)
+        db.session.commit()
     allTodo = Todo.query.all()
-    print(allTodo)
     return render_template('index.html',allTodo = allTodo)
 
-@app.route("/show")
-def show():
+@app.route('/delete/<int:sno>')
+def delete(sno):
+    todo = Todo.query.filter_by(sno=sno).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect("/")
+
+@app.route("/edit/<int:sno>")
+def edit(sno):
     allTodo = Todo.query.all()
     print(allTodo)
     return "<p>Welcome To Our Shopping Mart!</p>"
